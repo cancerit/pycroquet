c732ac82c65d4df8c6a43b1d5b1d2187a8549a19
Keiran Raine <kr2@sanger.ac.uk>
2021-12-01 14:45:14 +0000

    Bring test coverage up to scratch

--- tests/08_merge_test.py
+++ tests/08_merge_test.py
@@ -27,10 +27,88 @@
 # statement that reads ‘Copyright (c) 2005-2012’ should be interpreted as being
 # identical to a statement that reads ‘Copyright (c) 2005, 2006, 2007, 2008,
 # 2009, 2010, 2011, 2012’.
+import json
+import os
+import tempfile
+
 import pytest
 
 import pycroquet.merge as merge
 
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 
-def test_01_targets_guides_to_targets():
-    pass
+
+def test_01_merge_header_line():
+    h_line = merge.merge_header_line("command", "version", 1, "chksum")
+    assert h_line == "##Count-col-#1: chksum; version; command"
+
+
+def test_02_hash_file():
+    assert merge.hash_file(f"{DATA_DIR}/merge/bob_1.counts.tsv", "md5") == "md5: 41aacd3ea72f328b1d8ce6427ae6f28a"
+    assert (
+        merge.hash_file(f"{DATA_DIR}/merge/bob_1.counts.tsv", "sha256")
+        == "sha256: 05197cd393645e123b450b1fbda4fbaa4c49245f2d1ae3faf7f82269987b943a"
+    )
+
+
+@pytest.mark.parametrize(
+    "inputs, low_count, compare_to",
+    [
+        (
+            ["cli/output/bob_1.counts.tsv.gz", "cli/output/bob_2.counts.tsv.gz"],
+            None,
+            "merge/bob_1n2_nolow",
+        ),
+        (
+            ["cli/output/bob_1.counts.tsv.gz", "cli/output/bob_2.counts.tsv.gz"],
+            5,
+            "merge/bob_1n2_lowcount",
+        ),
+        (
+            ["cli/output/dual_low_count.counts.tsv.gz", "cli/output/dual_low_count_2.counts.tsv.gz"],
+            None,
+            "merge/dual_low",
+        ),
+    ],
+)
+def test_03_merge(inputs, low_count, compare_to):
+    checksum = "md5"
+    loglevel = "WARN"
+
+    stats_new = None
+
+    inputs = [os.path.join(DATA_DIR, input) for input in inputs]
+
+    with tempfile.TemporaryDirectory() as tdir:
+        output = os.path.join(tdir, "single")
+        merge.merge_counts(output, inputs, low_count, checksum, loglevel)
+
+        out_counts = f"{output}.merged-counts.tsv.gz"
+        out_stats = f"{output}.merged-stats.json"
+
+        assert os.path.exists(out_counts)
+        assert os.path.exists(out_stats)
+
+        with open(out_stats, "r") as sfp:
+            stats_new = json.load(sfp)
+
+    stats_old = None
+    with open(os.path.join(DATA_DIR, f"{compare_to}.merged-stats.json"), "r") as sfp:
+        stats_old = json.load(sfp)
+
+    for i in ("command", "version"):
+        del stats_old[i]
+        del stats_new[i]
+    assert stats_new == stats_old
+
+
+def test_04_samefile(capsys):
+    checksum = "md5"
+    loglevel = "WARN"
+
+    inputs = [os.path.join(DATA_DIR, "cli/output/bob_1.counts.tsv.gz") for i in range(0, 2)]
+
+    with tempfile.TemporaryDirectory() as tdir:
+        output = os.path.join(tdir, "single")
+        with pytest.raises(SystemExit):
+            merge.merge_counts(output, inputs, None, checksum, loglevel)
